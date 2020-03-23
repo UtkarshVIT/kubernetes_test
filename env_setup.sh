f2ea33e636e66d1c12eaf69b60aebe3db54e1edf
Utkarsh Sharma <utkarshsharma351@gmail.com>
2020-03-23 16:14:05 -0400

    adding env_setup.sh

---
+++ env_setup.sh
@@ -0,0 +1,5 @@
+#This is to setup env for minikube
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+#To check version do: kubectl version --client
